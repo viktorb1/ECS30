dfbae7c64b315981d04458cbc93a1bce75c4fe7a
viktorb1 <vbabchanik@ucdavis.edu>
2016-12-22 23:44:50 -0800

    emissions.c complete

--- prog3/emissions.c
+++ prog3/emissions.c
@@ -1,5 +1,60 @@
 #include <stdio.h>
 
+void checkifexceeds(int option, int odo, double grams_pm);
+
 int main() {
+	printf("(1) Carbon monoxide\n(2) Hydrocarbons\n(3) Nitrogen oxides\n(4) Nonmethane hydrocarbons\n");
+	int option, odo;
+	double grams_pm;
+	scanf("%d", &option);
+	printf("Enter number of grams emitted per mile>> ");
+	scanf("%lf", &grams_pm);
+	printf("Enter odometer reading>> ");
+	scanf("%d", &odo);
+	checkifexceeds(option, odo, grams_pm);
 	return 0;
+}
+
+void checkifexceeds(int option, int odo, double grams_pm) {
+	if (option == 1) {
+		if (odo > 50000 && grams_pm < 4.2) {
+			printf("Emissions do not exceed permitted level of 4.2 grams/mile.");
+		} else if (odo <= 50000 && grams_pm < 3.4) {
+			printf("Emissions do not exceed permitted level of 3.4 grams/mile.");
+		} else if (odo > 50000 && grams_pm > 4.2) {
+			printf("Emissions exceed permitted level of 4.2 grams/mile.");
+		} else  if (odo <= 50000 && grams_pm > 3.4) {
+			printf("Emissions exceed permitted level of 3.4 grams/mile.");
+		}
+	} else if (option == 2) {
+		if (odo > 50000 && grams_pm < 0.39) {
+			printf("Emissions do not exceed permitted level of 4.2 grams/mile.");
+		} else if (odo <= 50000 && grams_pm < 0.31) {
+			printf("Emissions do not exceed permitted level of 3.4 grams/mile.");
+		} else if (odo > 50000 && grams_pm > 0.39) {
+			printf("Emissions exceed permitted level of 4.2 grams/mile.");
+		} else  if (odo <= 50000 && grams_pm > 0.31) {
+			printf("Emissions exceed permitted level of 3.4 grams/mile.");
+		}
+	} else if (option == 3) {
+		if (odo > 50000 && grams_pm < 0.5) {
+			printf("Emissions do not exceed permitted level of 4.2 grams/mile.");
+		} else if (odo <= 50000 && grams_pm < 0.4) {
+			printf("Emissions do not exceed permitted level of 3.4 grams/mile.");
+		} else if (odo > 50000 && grams_pm > 0.5) {
+			printf("Emissions exceed permitted level of 4.2 grams/mile.");
+		} else  if (odo <= 50000 && grams_pm > 0.4) {
+			printf("Emissions exceed permitted level of 3.4 grams/mile.");
+		}
+	} else if (option == 4) {
+		if (odo > 50000 && grams_pm < 0.31) {
+			printf("Emissions do not exceed permitted level of 4.2 grams/mile.");
+		} else if (odo <= 50000 && grams_pm < 0.25) {
+			printf("Emissions do not exceed permitted level of 3.4 grams/mile.");
+		} else if (odo > 50000 && grams_pm > 0.31) {
+			printf("Emissions exceed permitted level of 4.2 grams/mile.");
+		} else  if (odo <= 50000 && grams_pm > 0.25) {
+			printf("Emissions exceed permitted level of 3.4 grams/mile.");
+		}
+	}
 }
